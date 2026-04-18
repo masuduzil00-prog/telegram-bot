c3ad36f4905f013137fcd5c586f07e674c25ebf7
masuduzil00-prog <masuduzil00@gmail.com>
2026-04-18 15:18:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler
 
-# /start command
 def start(update, context):
-
     keyboard = [
         [InlineKeyboardButton("🎬 বাচ্চাদের ভিডিও", url="https://t.me/+AYoYy8izLuM2NmY1")],
         [InlineKeyboardButton("🧸 বাচ্চাদের গ্রুপ", url="https://t.me/bacchader_Video_viral_video")],
@@ -13,19 +11,12 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text(
-        "👋 Welcome!\n\n"
-        "🧸 বাচ্চাদের জন্য সেরা ভিডিও কালেকশন এখানে 👇\n"
-        "🎬 প্রতিদিন নতুন ভিডিও আপডেট করা হয়\n\n"
-        "👇 নিচের বাটনে ক্লিক করে এখনই দেখে নিন",
+        "👋 Welcome!\n\n🧸 বাচ্চাদের ভিডিও এখানে 🎬\n👇 নিচে ক্লিক করুন",
         reply_markup=reply_markup
     )
 
-# BOT TOKEN (BotFather থেকে বসাবে)
 updater = Updater("8703192055:AAGuF9G8DhB3qV3RbKWCBdyLqJ8VxFZTJWs", use_context=True)
-
-# handler add
 updater.dispatcher.add_handler(CommandHandler("start", start))
 
-# start bot
 updater.start_polling()
-updater.idle()
+updater.idle()  
